81afc06395056f6efa6d82ce0e043430f10a6b4c
ozi-lix <ozilix@gmail.com>
2025-02-14 09:12:38 +0300

    Add files via upload

---
+++ appResource.js
@@ -0,0 +1,21 @@
+var res = {
+    productName: "SpreadJS Designer",
+    fileNotExist: 'File is not exist.',
+    saveFileError: 'Save fail: ',
+    openFileError: 'Open fail: ',
+    exportError: 'Export fail: ',
+    importError: 'Import fail: ',
+    menu: {
+        about: "About SpreadJS.Designer",
+        quit: "Quit SpreadJS.Designer",
+        edit: "Edit",
+        undo: "Undo",
+        redo: "Redo",
+        cut: "Cut",
+        copy: "Copy",
+        paste: "Paste",
+        selectAll: "Select All"
+    }
+}
+
+module.exports = res 
